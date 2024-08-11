24ef0f66a98027fbb8c61560de5fc964ab01920b
Tiyyagura Chandra Reddy <65798233+chandrareddyt21@users.noreply.github.com>
2024-08-11 13:56:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from langchain.callbacks.streamlit import StreamlitCallbackHandler
 import os
 
 
+
 # Arxiv and Wikipedia tools
 arxiv_wrapper = ArxivAPIWrapper(top_K_results=1, doc_content_chars_max=200)
 arxiv = ArxivQueryRun(api_wrapper=arxiv_wrapper)
